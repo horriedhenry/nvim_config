54473e0ad1efb535902ce46ff42089d1fc16d96a
horriedhenry <shivahenry33@gmail.com>
2023-11-07 17:32:16 +0530

    feat : colorscheme

--- lua/henry/core/colorscheme.lua
+++ lua/henry/core/colorscheme.lua
@@ -1,16 +1,16 @@
 require('rose-pine').setup({
-	disable_italics = true,
-	disable_background = false,
-	disable_float_background = true,
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		link = 'iris',
-		punctuation = 'subtle',
+    disable_italics = true,
+    disable_background = false,
+    disable_float_background = true,
+    --- @usage string hex value or named color from rosepinetheme.com/palette
+    groups = {
+        link = 'iris',
+        punctuation = 'subtle',
 
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
+        error = 'love',
+        hint = 'iris',
+        info = 'foam',
+        warn = 'gold',
 
         headings = {
             h1 = 'iris',
@@ -20,40 +20,40 @@ require('rose-pine').setup({
             h5 = 'pine',
             h6 = 'foam',
         },
-		-- or set all headings at once
-		-- headings = 'subtle'
-	},
+        -- or set all headings at once
+        -- headings = 'subtle'
+    },
 
-	-- Change specific vim highlight groups
-	-- https://github.com/rose-pine/neovim/wiki/Recipes
-	highlight_groups = {
-		-- ColorColumn = { bg = 'rose' },
+    -- Change specific vim highlight groups
+    -- https://github.com/rose-pine/neovim/wiki/Recipes
+    highlight_groups = {
+        -- ColorColumn = { bg = 'rose' },
 
-		-- Blend colours against the "base" background
-		CursorLine = { bg = 'foam', blend = 10 },
-		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-	}
+        -- Blend colours against the "base" background
+        CursorLine = { bg = 'foam', blend = 10 },
+        StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+    }
 })
 
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
+    background = {     -- :h background
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    transparent_background = true, -- disables setting the background color.
+    show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+    term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
+        enabled = false,           -- dims the background color of inactive window
         shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        percentage = 0.15,         -- percentage of the shade to apply to the inactive window
     },
-    no_italic = true, -- Force no italic
-    no_bold = true, -- Force no bold
-    no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
+    no_italic = true,              -- Force no italic
+    no_bold = true,                -- Force no bold
+    no_underline = false,          -- Force no underline
+    styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },   -- Change the style of comments
         conditionals = { "italic" },
         loops = {},
         functions = {},
@@ -80,58 +80,62 @@ require("catppuccin").setup({
 })
 
 require("tokyonight").setup({
-  transparent = false, -- Enable this to disable setting the background color
-  terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    comments = { italic = false },
-    keywords = { italic = false },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "transparent", -- style for sidebars, see below
-    floats = "transparent", -- style for floating windows
-  },
-  -- sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  sidebars = { "qf", "vista_kind", "terminal", "packer" },
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  on_colors = function(colors)
-    colors.hint = colors.orange
-    colors.error = "#ff0000"
-end;
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+    transparent = false,   -- Enable this to disable setting the background color
+    terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
+    styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "transparent", -- style for floating windows
+    },
+    -- sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+    sidebars = { "qf", "vista_kind", "terminal", "packer" },
+    -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+    on_colors = function(colors)
+        colors.hint = colors.orange
+        colors.error = "#ff0000"
+    end,
+    day_brightness = 0.3,           -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+    dim_inactive = false,           -- dims inactive windows
+    lualine_bold = false,           -- When `true`, section headers in the lualine theme will be bold
 
-  on_colors = function(colors) end,
+    on_colors = function(colors) end,
 
-  on_highlights = function(highlights, colors) end,
+    on_highlights = function(highlights, colors) end,
 })
 
 require("gruvbox").setup({
-  terminal_colors = true, -- add neovim terminal colors
-  undercurl = true,
-  underline = true,
-  bold = false,
-  italic = {
-    strings = false,
-    emphasis = false,
-    comments = false,
-    operators = false,
-    folds = false,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {
-      -- bright_red = "#f4a261"
-  },
-  dim_inactive = false,
-  transparent_mode = true,
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = false,
+    italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {
+        -- bright_red = "#f4a261"
+        -- FF6E4E // red
+        -- E74C3C // red (matches really good)
+        bright_red = "#E74C3C",
+        -- bright_orange = "#FFA500"
+    },
+    dim_inactive = false,
+    transparent_mode = true,
 })
 
 local status, _ = pcall(vim.cmd, "colorscheme gruvbox")
