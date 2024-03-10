3411d575e840064fd107f82c33f9b11709deb712
horriedhenry <shivahenry33@gmail.com>
2024-03-10 17:02:56 +0530

    harpoon

--- lua/henry/core/harpoon.lua
+++ lua/henry/core/harpoon.lua
@@ -4,9 +4,9 @@ local ui = require("harpoon.ui")
 local map = vim.keymap
 
 map.set("n", "<leader>a", mark.add_file)
-map.set("n", "<leader>h", ui.toggle_quick_menu)
+map.set("n", "<C-e>", ui.toggle_quick_menu)
 
 map.set("n", "<C-f>", function() ui.nav_file(1) end)
 map.set("n", "<C-s>", function() ui.nav_file(2) end)
-map.set("n", "<C-n>", function() ui.nav_file(3) end)
-map.set("n", "<C-t>", function() ui.nav_file(4) end)
+map.set("n", "<C-t>", function() ui.nav_file(3) end)
+map.set("n", "<C-n>", function() ui.nav_file(4) end)
