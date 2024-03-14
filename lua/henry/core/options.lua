b75dcf7d627db14e31849e207c49b2373a32edd5
horriedhenry <shivahenry33@gmail.com>
2024-03-14 17:36:36 +0530

    options

--- lua/henry/core/options.lua
+++ lua/henry/core/options.lua
@@ -24,8 +24,8 @@ vim.g.everforest_transparent_background = 0
 
 -- Gruvbox Material
 vim.g.gruvbox_material_foreground = "original"
--- vim.g.gruvbox_material_background = 'medium'
-vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_background = 'hard'
+vim.g.gruvbox_material_transparent_background = 0
 vim.g.gruvbox_material_float_style = "dim"
 -- vim.g.gruvbox_material_disable_terminal_colors = 1
 
