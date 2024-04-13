45f7c432c6652f95760838e47098ec42c4d2a289
horriedhenry <shivahenry33@gmail.com>
2024-04-13 15:02:00 +0530

    Trouble

--- lua/henry/plugins-setup.lua
+++ lua/henry/plugins-setup.lua
@@ -138,17 +138,4 @@ return packer.startup(function(use)
         require("packer").sync()
     end
 
-    -- Trouble
-
-    use({
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup {
-                icons = false,
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    })
 end)
