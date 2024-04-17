f0e7155575b549b56b087a7f59b01ab5cf26bd55
horriedhenry <shivahenry33@gmail.com>
2024-04-17 14:50:39 +0530

    trouble

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -247,12 +247,6 @@ local no_errors, error_msg = pcall(function()
             path = "/home/henry/.local/share/nvim/site/pack/packer/start/telescope.nvim",
             url = "https://github.com/nvim-telescope/telescope.nvim"
         },
-        ["trouble.nvim"] = {
-            config = { "\27LJ\2\nC\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\ftrouble\frequire\0" },
-            loaded = true,
-            path = "/home/henry/.local/share/nvim/site/pack/packer/start/trouble.nvim",
-            url = "https://github.com/folke/trouble.nvim"
-        },
         undotree = {
             loaded = true,
             path = "/home/henry/.local/share/nvim/site/pack/packer/start/undotree",
@@ -296,12 +290,6 @@ local no_errors, error_msg = pcall(function()
     }
 
     time([[Defining packer_plugins]], false)
-    -- Config for: trouble.nvim
-    time([[Config for trouble.nvim]], true)
-    try_loadstring(
-    "\27LJ\2\nC\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\ftrouble\frequire\0",
-        "config", "trouble.nvim")
-    time([[Config for trouble.nvim]], false)
     -- Load plugins in order defined by `after`
     time([[Sequenced loading]], true)
     vim.cmd [[ packadd nvim-treesitter ]]
