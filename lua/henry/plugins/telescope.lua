c04dff4884c6b3e77f40ce11bd807e7476f61083
horriedhenry <shivahenry33@gmail.com>
2024-04-17 14:51:55 +0530

    <C-q> now opens items in a quick fix list

--- lua/henry/plugins/telescope.lua
+++ lua/henry/plugins/telescope.lua
@@ -21,7 +21,6 @@ telescope.setup({
             i = {
                 ["<C-k>"] = actions.move_selection_previous,                       -- move to prev result
                 ["<C-j>"] = actions.move_selection_next,                           -- move to next result
-                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
             },
         }
     },
@@ -32,13 +31,12 @@ telescope.load_extension("fzf")
 local map = vim.keymap
 
 map.set("n", "<leader>f", "<cmd>Telescope find_files<cr>")
--- map.set("n", "<C-p>", "<cmd>Telescope find_files<cr>")
--- map.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
--- map.set("n", "<leader>c", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+map.set("n", "<leader>lg", "<cmd>Telescope live_grep<cr>")            -- find string in current working directory as you type
+map.set("n", "<leader>gs", "<cmd>Telescope grep_string<cr>")          -- find string under cursor in current working directory
 map.set("n", "<leader>b", "<cmd>Telescope buffers<cr>")               -- list open buffers in current neovim instance
-map.set("n", "<leader>H", "<cmd>Telescope help_tags<cr>")             -- list available help tags
-map.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>")          -- list available help tags
-map.set("n", "<leader>ds", "<cmd>Telescope lsp_document_symbols<cr>") -- list available help tags
+map.set("n", "<leader>H", "<cmd>Telescope help_tags<cr>")
+map.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>")
+map.set("n", "<leader>ds", "<cmd>Telescope lsp_document_symbols<cr>")
 
 local builtin = require('telescope.builtin')
 map.set("n", '<leader>ps', function()
