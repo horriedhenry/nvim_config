78c273aad385a451e8befdc127f698a309879eaa
horriedhenry <shivahenry33@gmail.com>
2024-04-19 16:59:30 +0530

    nothing

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local no_errors, error_msg = pcall(function()
 
     time([[Luarocks path setup]], true)
     local package_path_str =
-    "/home/henry/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?.lua;/home/henry/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?/init.lua;/home/henry/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?.lua;/home/henry/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?/init.lua"
-    local install_cpath_pattern = "/home/henry/.cache/nvim/packer_hererocks/2.1.1710088188/lib/lua/5.1/?.so"
+    "/home/henry/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?.lua;/home/henry/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?/init.lua;/home/henry/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?.lua;/home/henry/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?/init.lua"
+    local install_cpath_pattern = "/home/henry/.cache/nvim/packer_hererocks/2.1.1713484068/lib/lua/5.1/?.so"
     if not string.find(package.path, package_path_str, 1, true) then
         package.path = package.path .. ';' .. package_path_str
     end
