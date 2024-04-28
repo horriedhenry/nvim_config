ef488cda94f33ce5de1699f4c66198db6bf3cc59
horriedhenry <shivahenry33@gmail.com>
2024-04-29 00:36:23 +0530

    clang-format

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -182,5 +182,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.clang_format.with({
+            filetypes = { "c", "cpp", "objc", "objcpp" },
+            args = { "-style=file" }, -- Pass additional arguments if needed
+        }),
     },
 })
